55cb3d5069df855543c1dfb92bfdaa45d0d2dc97
AekkachaiRmuti <aekparker.0077@gmail.com>
2022-12-23 15:01:13 +0700

    dash

--- index.php
+++ index.php
@@ -7,9 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Louis Menu</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,300;1,300&display=swap" rel="stylesheet">
 
 </head>
 <style>
+*{
+    font-family: 'Kanit', sans-serif;
+}
     .rp {
         width: 10rem;
         align-items: center;
